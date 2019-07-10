91d90e94481d1e0a2877c8c5f502ee31998c19ec
lucasGabrielDeAA <lg.technus@gmail.com>
2019-07-10 16:47:27 -0300

    requestPermissions removed from index.js

--- index.js
+++ index.js
@@ -2,24 +2,8 @@
  * @format
  */
 
-import { AppRegistry, AsyncStorage, PermissionsAndroid } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-
-const requestPermissions = async () => {
-  const permissions = [
-    PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-    PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE
-  ];
-
-  const granted = await PermissionsAndroid.requestMultiple(permissions);
-
-  AsyncStorage.setItem('@App:LOCATION_PERMISSION', JSON.parse(granted["android.permission.ACCESS_FINE_LOCATION"] === PermissionsAndroid.RESULTS.GRANTED));
-  AsyncStorage.setItem('@App:PHONE_STATE_PERMISSION', JSON.parse(granted["android.permission.READ_PHONE_STATE"] === PermissionsAndroid.RESULTS.GRANTED));
-  AsyncStorage.setItem('@App:EXTERNAL_STORAGE_PERMISSION', JSON.parse(granted["android.permission.WRITE_EXTERNAL_STORAGE"] === PermissionsAndroid.RESULTS.GRANTED));
-}
-
-requestPermissions();
 
 AppRegistry.registerComponent(appName, () => App);
