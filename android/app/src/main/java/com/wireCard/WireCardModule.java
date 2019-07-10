6c5eb913e9ea61c8506eb81de675724a78e961e3
lucasGabrielDeAA <lg.technus@gmail.com>
2019-07-10 14:31:55 -0300

    SDK's init and SDK's checkMaquininhaStatus functionalities fixed

--- android/app/src/main/java/com/wireCard/WireCardModule.java
+++ android/app/src/main/java/com/wireCard/WireCardModule.java
@@ -1,6 +1,5 @@
 package com.wireCard;
 
-import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -10,8 +9,6 @@ import com.facebook.react.bridge.ReadableMapKeySetIterator;
 
 import android.app.Application;
 import android.app.Activity;
-
-import android.widget.Toast;
 
 import br.com.moip.authentication.Authentication;
 import br.com.moip.authentication.BasicAuth;
@@ -33,261 +30,148 @@ import br.com.moip.mpos.model.response.MposPaymentResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.Arrays;
 import java.util.List;
-
-import java.lang.Exception;
 
 import com.google.gson.Gson;
 
 public class WireCardModule extends ReactContextBaseJavaModule {
 
-    private final String TOKEN = "*** TOKEN ***";
-    private final String PASSWORD = "*** PASSWORD ***";
+  private ReactApplicationContext reactContext;
+  private Boolean SDKInitializated;
+  private Boolean maquininhaConnected;
+  private String status;
+  private Boolean statusCleared;
+  private Callback callback;
 
-    private Activity activity;
-    private ReactApplicationContext reactContext;
-    private Boolean SDKInitializated;
-    private Boolean maquininhaConnected;
-    private String status;
-    private Boolean statusCleared;
+  public WireCardModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+    this.status = null;
+  }
 
-    public WireCardModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-        this.status = null;
-        this.statusCleared = true;
-    }
+  @Override
+  public String getName() {
+    return "WireCard";
+  }
 
-    @Override
-    public String getName() {
-        return "WireCard";
-    }
+  @ReactMethod
+  public void getSDKStatus(Callback callback) {
+    callback.invoke(this.SDKInitializated);
+  }
 
-    @ReactMethod
-    public void getSDKStatus(Callback callback) {
-        callback.invoke(this.getSDKInitializated());
-    }
+  @ReactMethod
+  public void getMaquininhaStatus(Callback callback) {
+    callback.invoke(this.maquininhaConnected);
+  }
 
-    @ReactMethod
-    public void getMaquininhaStatus(Callback callback) {
-        callback.invoke(this.getMaquininhaConnected());
-    }
+  @ReactMethod
+  public void init(Callback callback) {
+    this.callback = callback;
+    MoipMpos.init(getCurrentActivity(), MoipMpos.Enviroment.SANDBOX, new BasicAuth(TOKEN, PASSWORD),
+        new InitCallback() {
+          public void onSuccess() {
+            SDKInitializated = true;
+            callback.invoke("SDK inicializado");
+          }
 
-    @ReactMethod
-    public void init(Callback callback) throws Exception {
-        this.setActivity(getCurrentActivity());
+          public void onError(MposError e) {
+            SDKInitializated = false;
+            callback.invoke(e.toString());
+          }
+        });
+  }
 
-        if (this.getActivity() == null) {
-            Toast.makeText(getReactApplicationContext(), "Erro de activity", Toast.LENGTH_LONG).show();
-        } else {
-            Authentication authentication = new BasicAuth(TOKEN, PASSWORD);
+  @ReactMethod
+  public void checkMaquininhaStatus(Callback callback) {
+    this.callback = callback;
 
-            if (authentication == null) {
-                Toast.makeText(getReactApplicationContext(), "Falha na autenticação", Toast.LENGTH_LONG).show();
-            } else {
-                setStatusCleared(false);
-                MoipMpos.init(activity, MoipMpos.Enviroment.SANDBOX, authentication, new InitCallback() {
-                    public void onSuccess() {
-                        setSDKInitializated(true);
-                        checkStatus(callback);
-                    }
+    MoipMpos.isPinpadConnected(getCurrentActivity(), new PinpadCallback() {
+      public void onSuccess() {
+        maquininhaConnected = true;
+        callback.invoke("Maquininha conectada");
+      }
 
-                    public void onError(MposError e) {
-                        setSDKInitializated(false);
-                        Gson gson = new Gson();
-                        callback.invoke(gson.toJson(e));
-                    }
-                });
-            }
+      public void onError(MposError e) {
+        maquininhaConnected = false;
+        callback.invoke(e.toString());
+      }
+
+      @Override
+      public void onActionChanged(MposAction action) {
+        status = action.toString();
+      }
+    });
+  }
+
+  @ReactMethod
+  public void charge(ReadableMap item, Callback callback) throws JSONException {
+    final Callback c = callback;
+
+    JSONObject object = convertMapToJson(item);
+
+    if (object != null) {
+      String description = String.valueOf(object.get("description"));
+      String details = String.valueOf(object.get("details"));
+      int quantity = (Integer) object.get("quantity");
+      int value = (Integer) object.get("value");
+      int type = (Integer) object.get("type");
+      int installment = (Integer) object.get("installment");
+
+      ItemRequest itemRequest = new ItemRequest(description, quantity, details, value);
+
+      List items = Arrays.asList(itemRequest);
+
+      MposPaymentRequest.Type transactionType = type == 1 ? MposPaymentRequest.Type.CREDIT
+          : MposPaymentRequest.Type.DEBIT;
+
+      MposPaymentRequest mposPaymentRequest = new MposPaymentRequest().installment(installment).type(transactionType)
+          .items(items);
+
+      MoipMpos.charge(getCurrentActivity(), mposPaymentRequest, new MposCallback() {
+        @Override
+        public void onActionChanged(MposAction action) {
+          status = action.toString();
+          statusCleared = false;
         }
-    }
 
-    @ReactMethod
-    public void checkMaquininhaStatus(Callback callback) {
-        this.setActivity(getCurrentActivity());
-
-        if (this.getActivity() == null) {
-            Toast.makeText(getReactApplicationContext(), "Erro de activity", Toast.LENGTH_LONG).show();
-        } else {
-            MoipMpos.isPinpadConnected(activity, new PinpadCallback() {
-                public void onSuccess() {
-                    setMaquininhaConnected(true);
-                    callback.invoke("Maquininha conectada");
-                }
-
-                public void onError(MposError e) {
-                    setMaquininhaConnected(false);
-                    callback.invoke(e.toString());
-                }
-
-                @Override
-                public void onActionChanged(MposAction action) {
-                    setStatus(action.toString());
-                    setStatusCleared(false);
-                }
-            });
+        @Override
+        public void onSuccess(MposPaymentResponse mposPaymentResponse) {
+          Gson gson = new Gson();
+          c.invoke(gson.toJson(mposPaymentResponse));
+          statusCleared = false;
         }
-    }
 
-    @ReactMethod
-    public void charge(ReadableMap item, Callback callback) throws JSONException {
-        this.setActivity(getCurrentActivity());
-
-        if (this.getActivity() == null) {
-            Toast.makeText(getReactApplicationContext(), "Erro de activity", Toast.LENGTH_LONG).show();
-        } else {
-            JSONObject object = convertMapToJson(item);
-
-            if (object != null) {
-                String description = String.valueOf(object.get("description"));
-                String details = String.valueOf(object.get("details"));
-                int quantity = (Integer) object.get("quantity");
-                int value = (Integer) object.get("value");
-                int type = (Integer) object.get("type");
-                int installment = (Integer) object.get("installment");
-
-                ItemRequest itemRequest = new ItemRequest(description, quantity, details, value);
-
-                List items = Arrays.asList(itemRequest);
-
-                MposPaymentRequest.Type transactionType = type == 1 ? MposPaymentRequest.Type.CREDIT
-                        : MposPaymentRequest.Type.DEBIT;
-
-                MposPaymentRequest mposPaymentRequest = new MposPaymentRequest().installment(installment)
-                        .type(transactionType).items(items);
-
-                MoipMpos.charge(this.activity, mposPaymentRequest, new MposCallback() {
-                    @Override
-                    public void onActionChanged(MposAction action) {
-                        setStatus(action.toString());
-                        setStatusCleared(false);
-                    }
-
-                    @Override
-                    public void onSuccess(MposPaymentResponse mposPaymentResponse) {
-                        Gson gson = new Gson();
-                        callback.invoke(gson.toJson(mposPaymentResponse));
-                        setStatusCleared(false);
-                    }
-
-                    public void onError(MposError e) {
-                        Gson gson = new Gson();
-                        callback.invoke(gson.toJson(e));
-                        setStatusCleared(false);
-                    }
-                });
-            }
+        public void onError(MposError e) {
+          Gson gson = new Gson();
+          c.invoke(gson.toJson(e));
+          statusCleared = false;
         }
+      });
     }
+  }
 
-    /**
-     * Método utilizado para ficar "escutando" as alterações de estado do SDK e/ou da conexão com a maquininha.
-     *
-     * @param callback
-     * @author Lucas Gabriel
-     * @since 26/04/2019
-     */
-    private void checkStatus(Callback callback) {
-        int delay = 0;
-        int interval = 100;
-        Timer timer = new Timer();
-        int timeout = 60000; // 60 seconds
-        long startTime = System.currentTimeMillis();
-
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                if (!getStatusCleared()) {
-                    setStatusCleared(true);
-                    callback.invoke(getStatus());
-                    timer.cancel();
-                } else if (System.currentTimeMillis() - startTime > timeout) {
-                    callback.invoke("timeout");
-                    timer.cancel();
-                }
-            }
-        }, delay, interval);
+  private static JSONObject convertMapToJson(ReadableMap map) throws JSONException {
+    JSONObject object = new JSONObject();
+    ReadableMapKeySetIterator iterator = map.keySetIterator();
+    while (iterator.hasNextKey()) {
+      String key = iterator.nextKey();
+      switch (map.getType(key)) {
+      case Null:
+        object.put(key, JSONObject.NULL);
+        break;
+      case Boolean:
+        object.put(key, map.getBoolean(key));
+        break;
+      case Number:
+        object.put(key, map.getInt(key));
+        break;
+      case String:
+        object.put(key, map.getString(key));
+        break;
+      }
     }
-
-    /**
-     * Método utilizado para extrair os dados recebidos do Javascript e transformar em um objeto de onde
-     * possam ser extraídos os valores necessários para as funcionalidades.
-     *
-     * @param map
-     * @return JSONObject
-     * @throws JSONException
-     */
-    private static JSONObject convertMapToJson(ReadableMap map) throws JSONException {
-        JSONObject object = new JSONObject();
-        ReadableMapKeySetIterator iterator = map.keySetIterator();
-        while (iterator.hasNextKey()) {
-            String key = iterator.nextKey();
-            switch (map.getType(key)) {
-            case Null:
-                object.put(key, JSONObject.NULL);
-                break;
-            case Boolean:
-                object.put(key, map.getBoolean(key));
-                break;
-            case Number:
-                object.put(key, map.getInt(key));
-                break;
-            case String:
-                object.put(key, map.getString(key));
-                break;
-            }
-        }
-        return object;
-    }
-
-    public void setActivity(Activity activity) {
-        this.activity = activity;
-    }
-
-    public Activity getActivity() {
-        return this.activity;
-    }
-
-    public void setReactcontext(ReactApplicationContext reactContext) {
-        this.reactContext = reactContext;
-    }
-
-    public ReactApplicationContext getReactContext() {
-        return this.reactContext;
-    }
-
-    public void setSDKInitializated(Boolean SDKInitializated) {
-        this.SDKInitializated = SDKInitializated;
-    }
-
-    public Boolean getSDKInitializated() {
-        return this.SDKInitializated;
-    }
-
-    public void setMaquininhaConnected(Boolean maquininhaConnected) {
-        this.maquininhaConnected = maquininhaConnected;
-    }
-
-    public Boolean getMaquininhaConnected() {
-        return this.maquininhaConnected;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public void setStatusCleared(Boolean statusCleared) {
-        this.statusCleared = statusCleared;
-    }
-
-    public Boolean getStatusCleared() {
-        return this.statusCleared;
-    }
+    return object;
+  }
 
 }
